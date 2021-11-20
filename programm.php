b2895f7b799f362d586e2874590f6eb3c9018db6
HindaMohammed <hindsadi65@gmail.com>
2021-11-20 14:41:00 +0200

    Project

---
+++ programm.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>programm</title>
+    <link rel="stylesheet" href="programm.css"/>
+</head>
+<body>
+        <?php include('header.html'); ?>
+    <div>
+        walaa
+           <p class="start_p" >This section will be devoted to the necessary software for the course material,
+        and we will work hard to provide the educational videos and easy ways to download them .</p>
+    
+<div id="programs">
+
+    
+    <div class="level">
+        <h3>Level 2</h3> 
+
+        <div class="signals">
+        <h4>Java LAB</h4>
+        <h4>Logic LAB</h4>
+        <h4>Circuits LAB</h4>
+        <h4>Electronics1 LAB</h4>
+        </div>
+
+    </div>
+    <div class="level">
+        <h3>Level 3</h3> 
+
+        <div class="signals">
+            <h4>Signals LAB  </h4>
+            <h4>LinearControlSystem LAB</h4>
+            <h4>DataBase LAB</h4>
+
+        </div>
+
+    </div>
+    <div class="level">
+        <h3>Level 4</h3> 
+
+        <div class="signals">
+            <h4>Assembly LAB</h4>
+            <h4>OS LAB</h4>
+            <h4>DataCom LAB</h4>
+
+        </div>
+
+
+    </div>
+
+    <div class="level">
+        <h3>Level 5</h3> 
+
+        <div class="signals">
+            <h4>web LAB</h4>
+
+
+        </div>
+
+</div>
+</div>
+    </div>
+        <?php include('footer.html'); ?>
+
+</body>
+</html>
